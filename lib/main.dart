c4a2a9d809ebaf9308702ad0ca19620c2c0659b4
Odhi-ambo <michaelderrick605@gmail.com>
2024-12-25 09:41:48 +0300

    root file

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(create: (context) => WorkoutData());
+    return ChangeNotifierProvider(create: (context) => WorkoutData(),
+    child: MaterialApp(
+      debugShowCheckedModeBanner: false,
+      
+      home: HomePage()
+    ));
   }
 }
