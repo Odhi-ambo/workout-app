b4f9883255afe8a2f24604e058ca9837c114513e
Odhi-ambo <michaelderrick605@gmail.com>
2024-12-26 11:19:58 +0300

    initial commit body

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,21 +5,25 @@ import 'package:work_out_app/data/workout_data.dart';
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  void createNewWorkout(){}
+  void createNewWorkout() {}
 
   @override
   Widget build(BuildContext context) {
     return Consumer<WorkoutData>(
-      builder: (context, value,child)=> Scaffold(
+      builder: (context, value, child) => Scaffold(
         appBar: AppBar(
           title: const Text("Workout Tracker"),
         ),
-        floatingActionButton: FloatingActionButton(onPressed: createNewWorkout),
+        floatingActionButton: FloatingActionButton(
+          onPressed: createNewWorkout,
+          child: Icon(Icons.add),
+        ),
         body: ListView.builder(
           itemCount: value.getWorkoutList().length,
           itemBuilder: (context, index) => ListTile(
             title: Text(value.getWorkoutList()[index].name),
-          ),),
+          ),
+        ),
       ),
     );
   }
