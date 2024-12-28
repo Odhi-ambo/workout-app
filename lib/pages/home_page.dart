c0babd0a86eb3b33879aa0bbd013a8ce7fde70b4
Odhi-ambo <michaelderrick605@gmail.com>
2024-12-28 08:27:39 +0300

    initial commit body

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -44,7 +44,7 @@ class _HomePageState extends State<HomePage> {
     return Consumer<WorkoutData>(
       builder: (context, value, child) => Scaffold(
         appBar: AppBar(
-          title: const Center(child:  Text("M & B Fitness Galore")),
+          title: const Center(child: Text("M & B Fitness Cenda")),
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: createNewWorkout,
